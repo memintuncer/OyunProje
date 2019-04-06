5d6169e77d72ebe3148b5b81e57e4a4d2652228d
memintuncer <memin11235@gmail.com>
2019-04-06 12:07:05 +0300

    level1in çok basit hali
    Kapıları ayarlamadım,
    Mekanlar boş,
    Biraz daha asset araştırdım ama onları indiremedim sonra indircem,
    Tuzaklar koymadım ekleyeceğim daha sonra,
    Puzzle kısımları da yapılmadı.

---
+++ New Unity Project (2)/Assets/Scripts/TBdoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TBdoor : MonoBehaviour
+{
+    Transform target;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
