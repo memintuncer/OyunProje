1c3c55d3b6371595f42dbcda00a32ab8727dadf9
memintuncer <memin11235@gmail.com>
2019-04-16 16:03:11 +0300

    silah ve mixamo

--- New Unity Project (2)/Assets/Yurowm/Demo/Scripts/Actions.cs
+++ New Unity Project (2)/Assets/Yurowm/Demo/Scripts/Actions.cs
@@ -1,32 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Animator))]
-public class Actions : MonoBehaviour
-{
+[RequireComponent (typeof (Animator))]
+public class Actions : MonoBehaviour {
 
-    private Animator animator;
+	private Animator animator;
 
-    const int countOfDamageAnimations = 3;
-    int lastDamageAnimation = -1;
+	const int countOfDamageAnimations = 3;
+	int lastDamageAnimation = -1;
 
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
 
-    public void Stay()
-    {
-        animator.SetBool("Aiming", false);
-        animator.SetFloat("Speed", 0f);
-    }
+	public void Stay () {
+		animator.SetBool("Aiming", false);
+		animator.SetFloat ("Speed", 0f);
+		}
 
-    public void Walk()
-    {
-        animator.SetBool("Aiming", false);
-        animator.SetFloat("Speed", 0.5f);
-    }
-
+	public void Walk () {
+		animator.SetBool("Aiming", false);
+		animator.SetFloat ("Speed", 0.5f);
+	}
     public void WalkAndFire()
     {
         animator.SetFloat("Speed", 0.5f);
@@ -34,56 +29,49 @@ public class Actions : MonoBehaviour
         animator.SetTrigger("Attack");
     }
 
-    public void Run()
-    {
-        animator.SetBool("Aiming", false);
-        animator.SetFloat("Speed", 1f);
-    }
+    public void Run () {
+		animator.SetBool("Aiming", false);
+		animator.SetFloat ("Speed", 1f);
+	}
 
-    public void Attack()
-    {
-        Aiming();
-        animator.SetTrigger("Attack");
-    }
+	public void Attack () {
+		Aiming ();
+		animator.SetTrigger ("Attack");
+	}
 
-    public void Death()
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
-            animator.Play("Idle", 0);
-        else
-            animator.SetTrigger("Death");
-    }
+	public void Death () {
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death"))
+			animator.Play("Idle", 0);
+		else
+			animator.SetTrigger ("Death");
+	}
 
-    public void Damage()
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death")) return;
-        int id = Random.Range(0, countOfDamageAnimations);
-        if (countOfDamageAnimations > 1)
-            while (id == lastDamageAnimation)
-                id = Random.Range(0, countOfDamageAnimations);
-        lastDamageAnimation = id;
-        animator.SetInteger("DamageID", id);
-        animator.SetTrigger("Damage");
-    }
+	public void Damage () {
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death")) return;
+		int id = Random.Range(0, countOfDamageAnimations);
+		if (countOfDamageAnimations > 1)
+			while (id == lastDamageAnimation)
+				id = Random.Range(0, countOfDamageAnimations);
+		lastDamageAnimation = id;
+		animator.SetInteger ("DamageID", id);
+		animator.SetTrigger ("Damage");
+	}
 
-    public void Jump()
-    {
-        animator.SetBool("Squat", false);
-        animator.SetFloat("Speed", 0f);
-        animator.SetBool("Aiming", false);
-        animator.SetTrigger("Jump");
-    }
+	public void Jump () {
+		animator.SetBool ("Squat", false);
+		animator.SetFloat ("Speed", 0f);
+		animator.SetBool("Aiming", false);
+		animator.SetTrigger ("Jump");
+	}
 
-    public void Aiming()
-    {
-        animator.SetBool("Squat", false);
-        animator.SetFloat("Speed", 0f);
-        animator.SetBool("Aiming", true);
-    }
+	public void Aiming () {
+		animator.SetBool ("Squat", false);
+		animator.SetFloat ("Speed", 0f);
+		animator.SetBool("Aiming", true);
+	}
 
-    public void Sitting()
-    {
-        animator.SetBool("Squat", !animator.GetBool("Squat"));
-        animator.SetBool("Aiming", false);
-    }
+	public void Sitting () {
+		animator.SetBool ("Squat", !animator.GetBool("Squat"));
+		animator.SetBool("Aiming", false);
+	}
 }
---
+++ New Unity Project (2)/Assets/door_open.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_open : MonoBehaviour
+{
+    public GameObject target;
+   
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (target.transform.position.z) - (transform.position.z); 
+        if (distance < 0.1f)
+        {
+            anim.SetBool("character_nearby", true);
+        } 
+       
+    }
+}
